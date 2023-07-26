7489325b56b4503d9e7e7c413d658ce179806d09
luv4shy <plmyh@hotmail.com>
2023-07-26 21:26:23 +0800

    Update index.js

--- projects/pego/index.js
+++ projects/pego/index.js
@@ -1,20 +1,20 @@
-const MINT_TOKEN_CONTRACT = '0x1f3Af095CDa17d63cad238358837321e95FC5915';
-const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
+const PG_TOKEN_CONTRACT = '0x7bb8bB3e4F3f8C40D4ab3bA07622453c8d8CC949';
+// const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
 
 async function tvl(_, _1, _2, { api }) {
   const collateralBalance = await api.call({
     abi: 'erc20:balanceOf',
-    target: MINT_TOKEN_CONTRACT,
-    params: [MINT_CLUB_BOND_CONTRACT],
+    target: PG_TOKEN_CONTRACT,
+   // params: [MINT_CLUB_BOND_CONTRACT],
   });
 
-  api.add(MINT_TOKEN_CONTRACT, collateralBalance)
+  api.add(PG_TOKEN_CONTRACT, collateralBalance)
 }
 
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
-  methodology: 'counts the number of MINT tokens in the Club Bonding contract.',
+  methodology: 'counts the number of PG tokens in the PEGO contract.',
   start: 1000235,
   bsc: {
     tvl,
